b32c8ae19475ea7f3dfa47f097fe3350f3f85b35
Dio6o <lohn.diogo@gmail.com>
2026-06-01 22:19:54 -0300

    php web

---
+++ .idea/web/php/rodape.infob.php
@@ -0,0 +1,8 @@
+<footer>
+    <p>&copy; Todos os direitos reservados</p>
+    <form action="newsletter.php" method="post">
+        <input type="text" name="nome" placeholder="nome">
+        <input type="email" name="email" placeholder="email">
+        <input type="submit" value="cadastrar">
+    </form>
+</footer>
